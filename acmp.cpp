5e70b5311380dc38cf62b8c921f004f0824d799c
zaharuus22 <berlovzahar@gmail.com>
2026-03-29 20:36:59 +0700

    старт

---
+++ acmp.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+std::ifstream ent("input.txt");
+std::ofstream esc("output.txt");
+
+int input();
+int output();
+int solve();
+int input() {
+
+    
+    return 0;
+}
+
+int solve() {
+   
+
+
+
+
+    return 0;
+}
+
+
+
+int output() {
+
+
+    return 0;
+}
+
+int main()
+{
+
+    input();
+    solve();
+    output();
+    return 0;
+}
