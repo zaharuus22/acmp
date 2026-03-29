2a042038df9bf6dab5773567a6ae66c3f9414d81
zaharuus22 <berlovzahar@gmail.com>
2026-03-29 21:33:34 +0700

    A4

--- acmp.cpp
+++ acmp.cpp
@@ -3,21 +3,42 @@
 using namespace std;
 std::ifstream ent("input.txt");
 std::ofstream esc("output.txt");
-
+//#define ent cin
+//#define esc cout
 int input();
+int a;
+bool ans;
 int output();
 int solve();
 int input() {
 
-    
+    ent >> a;
     return 0;
 }
 
 int solve() {
    
-
-
-
+    /*
+    1 n 
+    3 n
+    4 y
+    5 n
+    6 n
+    */
+    if (a % 2 == 1) {
+        ans = 0;
+        return 0;
+    }
+    if (a == 2) {
+        ans = 0;
+        return 0;
+    }
+    if (a % 2 == 0) {
+        ans = 1;
+    }
+    else {
+        ans = 0;
+    }
 
     return 0;
 }
@@ -25,8 +46,13 @@ int solve() {
 
 
 int output() {
+    if (ans) {
+        esc << "YES";
 
-
+    }
+    else {
+        esc << "NO";
+    }
     return 0;
 }
 
