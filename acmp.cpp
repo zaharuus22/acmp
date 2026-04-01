af1568e0dee3c47c9a2dd635a8315c2cc974e8b0
zaharuus22 <berlovzahar@gmail.com>
2026-04-01 14:10:18 +0700

    1

--- acmp.cpp
+++ acmp.cpp
@@ -1,20 +1,31 @@
 ﻿#include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
+#ifdef _DEBUG
 std::ifstream ent("input.txt");
 std::ofstream esc("output.txt");
+#define ent cin
+#define esc cout
+#endif
 
+string ms[103];
+long long n;
 int input();
 int output();
 int solve();
 int input() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
 
-    
+        cin >> ms[i];
+        ms[i].size();
+    }
     return 0;
 }
 
 int solve() {
-   
+
 
 
 
