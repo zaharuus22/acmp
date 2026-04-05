dae18a61ec009479f0a154b506e83b6dd82a2053
zaharuus22 <berlovzahar@gmail.com>
2026-04-05 19:36:58 +0700

    Update acmp.cpp

--- acmp.cpp
+++ acmp.cpp
@@ -1,20 +1,25 @@
 ﻿#include <iostream>
 #include <fstream>
 using namespace std;
-#ifdef _DEBUG
-
-
-std::ifstream cin("input.txt");
-std::ofstream cout("output.txt");
-#endif
+std::ifstream ent("input.txt");
+std::ofstream esc("output.txt");
+long long n, m;
+long long ms[1009];
+long long ans = 0;
 int input();
 int output();
 int solve();
 int input() {
+    ent >> n >> m;
+    for (int i = 0; i < n; i++) {
+        ent >> ms[i];
 
+
+    }
     
     return 0;
 }
+
 
 int solve() {
    
