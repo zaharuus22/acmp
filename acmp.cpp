2019c90b961c52d1f1b3bbfad2b6b929be5b48df
zaharuus22 <berlovzahar@gmail.com>
2026-04-05 20:40:00 +0700

    1

--- acmp.cpp
+++ acmp.cpp
@@ -3,46 +3,73 @@
 using namespace std;
 std::ifstream ent("input.txt");
 std::ofstream esc("output.txt");
-long long n, m;
+long long n, m, a, b;
 long long ms[1009];
 long long ans = 0;
 int input();
 int output();
 int solve();
+int bubble();
 int input() {
-    ent >> n >> m;
-    for (int i = 0; i < n; i++) {
-        ent >> ms[i];
+	ent >> n >> m;
+	for (int i = 0; i < n; i++) {
+		ent >> ms[i];
 
 
-    }
-    
-    return 0;
+	}
+
+	return 0;
+}
+int bubble() {
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < (n - 1); j++) {
+			if (ms[j] < ms[j + 1]) {
+				a = ms[j];
+				b = ms[j + 1];
+				ms[j] = b;
+				ms[j + 1] = a;
+
+			}
+
+		}
+
+
+	}
+	return 0;
 }
 
 
 int solve() {
-   
+
+	for (int i = 0; i < m; i++) {
+		if (ms[i] < 0) {
+
+			break;
+		}
+
+		ans += ms[i];
+	}
 
 
 
-
-    return 0;
+	return 0;
 }
 
 
 
 int output() {
 
-
-    return 0;
+	esc << ans;
+	return 0;
 }
 
 int main()
 {
 
-    input();
-    solve();
-    output();
-    return 0;
+	input();
+	bubble();
+	solve();
+	output();
+	return 0;
 }
