1057e44559507e84d66e5ec5a626293db8a38b05
zaharuus22 <berlovzahar@gmail.com>
2026-04-05 21:52:22 +0700

    6

--- acmp.cpp
+++ acmp.cpp
@@ -2,42 +2,78 @@
 #include <fstream>
 using namespace std;
 #ifdef _DEBUG
+#define cin ent
+#define cout esc
 
-
-std::ifstream cin("input.txt");
-std::ofstream cout("output.txt");
+std::ifstream ent("input.txt");
+std::ofstream esc("output.txt");
 #endif
+long long n, m;
+long long s;
+long long ms[1009][1009];
 int input();
 int output();
 int solve();
 int input() {
-
-    
-    return 0;
+	cin >> n >> m;
+	
+	return 0;
 }
+int elder(long long ans, long long dg) {
+	if (ans % 10 == dg) {
+		s++;
 
+	}
+
+
+}
+int safik(int dg) {
+	for (int x = 1; x <= n; x++) {
+		for (int y = 1; y <= m; y++) {
+
+			
+
+		}
+
+
+	}
+
+
+
+}
 int solve() {
-   
+	for (int x = 1; x <= n ; x++) {
+		for (int y = 1; y <= m; y++) {
+
+			ms[x][y] = x * y;
+
+		}
+
+
+	}
+	for (int dg = 0; dg <= 9; dg++) {
+		safik(dg);
+
+	}
 
 
 
-
-    return 0;
+	return 0;
 }
 
 
 
 int output() {
+	
 
-
-    return 0;
+	return 0;
 }
 
 int main()
 {
 
-    input();
-    solve();
-    output();
-    return 0;
+	input();
+	solve();
+	output();
+	return 0;
 }
