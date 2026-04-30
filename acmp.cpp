55d0f9ec7cc5eed263cf8c0a40ae5026781ad5f0
zaharuus22 <berlovzahar@gmail.com>
2026-04-30 17:49:29 +0700

    1

--- acmp.cpp
+++ acmp.cpp
@@ -8,37 +8,53 @@ using namespace std;
 std::ifstream ent("input.txt");
 std::ofstream esc("output.txt");
 #endif
+int n;
+int ms[10000];
+int s = 0;
 int input();
 int output();
 int solve();
 int input() {
-    
-    
-    return 0;
+	cin >> n;
+
+	return 0;
 }
 
 int solve() {
-   
+
+
+		
+	
+		for (int i = 9; i >= 2; i--) {
+			while (n % i == 0) {
+				ms[s] = i;
+				s++;
+				n = n / i;
+
+			}
+
+
+		}
+		cout << ms[0] << ms[1];
+	
 
 
 
-
-    return 0;
+	return 0;
 }
 
 
 
 int output() {
 
-
-    return 0;
+	return 0;
 }
 
 int main()
 {
 
-    input();
-    solve();
-    output();
-    return 0;
+	input();
+	solve();
+	output();
+	return 0;
 }
